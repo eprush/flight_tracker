cfbae5d28ce75e51675433302747b3d10352b486
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-04-07 21:52:41 +0300

    add bounds at config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import datetime
+
 # Define your API token and the endpoint URL
 API_TOKEN = 'beb8590873bff5b83d2972f1aee16234'
 BASE_URL = 'https://api.aviationstack.com'
@@ -10,4 +12,12 @@ url = f"{BASE_URL}{ENDPOINT}"
 # Define any query parameters, if needed (optional)
 params = {
     "access_key": API_TOKEN  # Example coordinates
+}
+
+#the Black Sea area
+bounds = {
+    "min_longitude": 27.77325,
+    "max_longitude": 42.11512,
+    "min_latitude": 40.8274,
+    "max_latitude": 46.93029,
 }
--- parsing/aviastack_parser.py
+++ parsing/aviastack_parser.py
@@ -1,42 +1,58 @@
 from typing import Iterable, Sized
 from parsing.abstract_parser import AbstractParser, T
 from models.flight_data import Data
+from config import bounds
 
+def is_in_bounds(lat, lon):
+    return bounds["min_latitude"] <= lat <= bounds["max_latitude"] and bounds["min_longitude"] <= lon <= bounds["max_longitude"]
 
 class AviationStackParser(AbstractParser):
     def __init__(self, seq: dict):
         self.data: Iterable | Sized = seq["data"]
 
-
     def get(self) -> tuple[T, ...]:
-        result = tuple(Data(
-            airline="",
-            code_icao="",
-            aircraft="",
-            lat=float("inf"),
-            lon=float("inf")
-            )
-            for _ in range(len(self.data)))
-
+        result = []
         for i, flight in enumerate(self.data):
-            flight_data = result[i]
+            flight_data = Data(aircraft="", airline="", code_icao="", lat=float("inf"), lon=float("inf"), callsign="")
             try:
-                aircraft = flight["aircraft"]["modelText"]
-                code_icao = flight["flight"]["icao"]
-                airline = flight["airline"]["name"] if flight["airline"]["name"] \
-                    else flight["flight"]["codeshared"]["airline_name"]
-                # callsign = flight[]
                 lat = flight["live"]["latitude"]
+                print(lat)
                 lon = flight["live"]["longitude"]
+                print(lon)
+
+                aircraft = flight["aircraft"]["registration"]
+                print(aircraft)
+
+                code_icao = flight["flight"]["icao"]
+                print(code_icao)
+
+                airline = flight["airline"]["name"] if flight["airline"] and flight["airline"]["name"] \
+                    else flight["flight"]["codeshared"]["airline_name"]
+                print(airline)
             except KeyError:
+                print("KeyError")
                 continue
             except TypeError:
+                print("TypeError")
                 continue
 
+            try:
+                callsign = flight["callsign"]
+                callsign = callsign if callsign is not None else ""
+                flight_data.callsign = callsign
+                print(callsign)
+            except KeyError:
+                print("Сallsign not found")
+
+            lat = float(lat) if lat is not None else float("inf")
+            lon = float(lon) if lon is not None else float("inf")
+            if not is_in_bounds(lat, lon):
+                continue
+
+            flight_data.lat = lat
+            flight_data.lon = lon
             flight_data.aircraft = aircraft if aircraft is not None else ""
             flight_data.code_icao = code_icao if code_icao is not None else ""
             flight_data.airline = airline if airline is not None else ""
-            flight_data.lat = float(lat) if lat is not None else float("inf")
-            flight_data.lon = float(lon) if lon is not None else float("inf")
-            #flight_data.callsign = callsign
-        return result
+            result.append(flight_data)
+        return tuple(result)
