61632ea02d0d74abadacc9cfda20592a0fa249cf
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-04-10 21:13:30 +0300

    change repository class to PostgreSQLRepository in main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from loading import load_data
 from parsing.aviastack_parser import AviationStackParser
-from repositories.csv_repository import CSVRepository
+#from repositories.csv_repository import CSVRepository
+from repositories.postgresql_repository import PostgreSQLRepository
 from models.flight_data import FlightData
-from config import params_iterator
+from config import params_iterator, postgreSQL_db_name
 
 if __name__ == "__main__":
-    repo = CSVRepository("aviastack.csv", FlightData)
+    repo = PostgreSQLRepository(postgreSQL_db_name, FlightData)
     for params in params_iterator:
         data = load_data(params)
         parser = AviationStackParser(data)
